3b40e73c9bdedf5d3446152132bad9e0c7640e5e
Lesly Sanchez <leslyf.sanchezm@gmail.com>
2019-09-24 00:49:14 -0500

    lives countre added; code stopped working when linking .js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// This is my array of letters to guess
+// var letterChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+// "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var toGuess = [Math.ceil(Math.random() * 1000)];
+console.log (toGuess);
+
+
+function playerTryToGuess() {
+            
+            var guess = document.getElementById("playerGuess")["0"].value;
+}
+        
