93e13ff191ef1ed4d99ca19f8bbeb483fb2b0aeb
Lesly Sanchez <leslyf.sanchezm@gmail.com>
2019-09-24 18:06:40 -0500

    TextContent added to the conditions, lives counter and letters already played working now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,46 @@
-// This is my array of letters to guess
-// var letterChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-// "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        // Define the alphabet array
+        var letterChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var toGuess = [Math.ceil(Math.random() * 1000)];
-console.log (toGuess);
-
-
-function playerTryToGuess() {
-            
+        var lettersPlayed = [''];
+        var lettersPlayedText=document.getElementById("lettersPlayed");
+            lettersPlayedText.textContent = lettersPlayed;
+        
+        // player will have only 15 lives to guess the right letter
+        var lives = 0;
+        var livesText = document.getElementById ("lives");
+            livesText.textContent = lives;
+ 
+        // This will generate the random letter for the player to guess - why it does not work when I declare it as a function? 
+        // Why it generates a number instead of a letter?
+        
+        var toGuess = letterChoice[Math.floor(Math.random() * letterChoice.length)];
+            console.log (toGuess);        
+        
+        // ***How the game works:
+        function playerTryToGuess() {
+            // player will enter the letter is guessing
             var guess = document.getElementById("playerGuess")["0"].value;
-}
+                console.log(guess);
+            
+
+            // compare user guess vs random letter
+            // check if wrong...
+            if (guess !== toGuess){
+                lettersPlayed.push(guess);
+                console.log (lettersPlayed);
+                if (lettersPlayed.length > 14){
+                    alert ("you are not a mind reader")
+                }
+                
+            }
+            // check if right...
+            else if (guess === toGuess){
+                alert("WOW! you are a great mind reader");
+
+            }
+
+            var guess = document.getElementById("playerGuess")["0"].select();      
+    
+
         
